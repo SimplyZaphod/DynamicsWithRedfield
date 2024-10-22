8a4aad93707ada4771f7166fafcdc150d97766ec
Lorenzo Savi <s.savi.lorenzo@gmail.com>
2024-10-22 15:35:28 +0200

    New compile options

--- CompileEvol.sh
+++ CompileEvol.sh
@@ -1,6 +1,7 @@
 ifort -c InputOutput.f90 -traceback -qopenmp
 ifort -c MathFunc.f90 -traceback -qopenmp
 ifort -c conversion.f90 -traceback -qopenmp
-ifort -c Redfieldmodule.f90 -traceback -qopenmpEvolutionModules.f90 -traceback -qopenmp
+ifort -c Redfieldmodule.f90 -traceback -qopenmp
+ifort -c EvolutionModules.f90 -traceback -qopenmp
 ifort RedfieldDynamicsGeneral.f90 -o RedfieldDynamicsGeneral.e -llapack MathFunc.o conversion.o InputOutput.o EvolutionModules.o Redfieldmodule.o -traceback -qopenmp
 ifort CorrelationFunctionGeneral.f90 -o CorrelationFunctionGeneral.e -llapack MathFunc.o conversion.o InputOutput.o EvolutionModules.o Redfieldmodule.o -traceback -qopenmp
